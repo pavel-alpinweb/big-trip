bd7cdefc051bbf0d70de215d48dc1a689702d397
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-08-24 11:55:36 +0600

    clean code

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11,9 +11,6 @@ const deferenceBetweenDays = (from, to) => {
   const diffMilliseconds = dateTo.diff(dateFrom, 'millisecond');
   const durationDates = dayjs.duration(diffMilliseconds);
 
-  // eslint-disable-next-line no-console
-  console.log('duration', dayjs.duration(diffMilliseconds));
-
   if (durationDates.$d.days > 0) {
     return `${durationDates.$d.days}D ${durationDates.$d.hours}H ${durationDates.$d.minutes}M`;
   } else if (durationDates.$d.hours > 0) {
