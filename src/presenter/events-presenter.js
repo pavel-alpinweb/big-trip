b8d24e6b48659a230936f09d51942eb49d365367
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-08-28 19:15:01 +0600

    create empty message

--- src/presenter/events-presenter.js
+++
@@ -1,8 +0,0 @@
-import {render, RenderPosition} from '../render.js';
-import Sort from '../view/sort.js';
-
-export default class EventsPresenter {
-  init(eventsContainer) {
-    render(new Sort, eventsContainer, RenderPosition.AFTERBEGIN);
-  }
-}
---
+++ src/presenter/sort-presenter.js
@@ -0,0 +1,19 @@
+import {render, RenderPosition} from '../render.js';
+import Sort from '../view/sort.js';
+
+
+export default class SortPresenter {
+  #eventsModel = null;
+  #listContainer = null;
+
+  constructor(eventsModel, listContainer) {
+    this.#eventsModel = eventsModel;
+    this.#listContainer = listContainer;
+  }
+
+  init(eventsModel, eventsContainer) {
+    if (this.#eventsModel.points.length > 0) {
+      render(new Sort, this.#listContainer, RenderPosition.AFTERBEGIN);
+    }
+  }
+}
