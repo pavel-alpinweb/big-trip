632bbd6545e65edad58971be3b1d2920cdd2b3a3
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-09-30 13:37:39 +0600

    clear code

--- src/main.js
+++ src/main.js
@@ -7,15 +7,11 @@ import ListPresenter from './presenter/list-presenter.js';
 const eventsModel = new EventsModel();
 
 // Containers
-// const eventsContainer = document.querySelector('.trip-events');
 const listContainer = document.querySelector('.trip-events__list');
 
 // Presenters
-// const sortPresenter = new SortPresenter(eventsModel, eventsContainer);
 const listPresenter = new ListPresenter(eventsModel, listContainer);
 
-// filtersPresenter.init();
-// sortPresenter.init();
 listPresenter.init();
 
 // eslint-disable-next-line
